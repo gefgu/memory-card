27ac60d626d1bf1f0cfde5c438fc752609e64905
gefgu <gefgu@hotmail.com>
2022-04-29 16:19:21 -0300

    Add heading text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,19 @@
 function Header(props) {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Cosmere's Memory Game</h1>
+      <div className="scoreboard">
+        <p>Score: 0</p>
+        <p>Best Score: 0</p>
+      </div>
+      <div>
+        <p>
+          Get points by clicking on an image, but don't click on any more than
+          once!
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
